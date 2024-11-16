08512d095e265a8203f14b5a231efe82e1875712
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2024-11-16 13:14:06 +0100

    feat: add banner component

---
+++ src/components/TopBanner.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import React from "react";
+
+
+interface TopBanner {
+    title: string;
+    description: string;
+    buttonText: string;
+    buttonLink: string;
+}
+
+/**
+ * Creates a modern banner for the top a website
+ * @param title The title of the banner. It will be bold
+ * @param description The description of the banner. It will be normal
+ * @param buttonText The text for the action button
+ * @param buttonLink The link for the action button
+ * @constructor
+ */
+const TopBanner: React.FC<TopBanner> = ({ title, description, buttonText, buttonLink }) => {
+    const [isVisible, setIsVisible] = React.useState(true);
+
+    if (!isVisible) return null;
+
+    return (
+        <div
+            className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+            <div
+                className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+                aria-hidden="true">
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+                    }}
+                    className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+                />
+            </div>
+            <div
+                className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+                aria-hidden="true">
+                <div
+                    style={{
+                        clipPath:
+                            'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+                    }}
+                    className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+                />
+            </div>
+            <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+                <p className="text-sm/6 text-gray-900">
+                    <strong className="font-semibold">{title}</strong>
+                    <svg viewBox="0 0 2 2" className="mx-2 inline size-0.5 fill-current" aria-hidden="true">
+                        <circle cx="1" cy="1" r="1"/>
+                    </svg>
+                    {description}
+                </p>
+                <a href={buttonLink}
+                   className="flex-none rounded-full bg-gray-900 py-1 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">
+                    {buttonText}&nbsp;
+                    <span aria-hidden="true">&rarr;</span>
+                </a>
+            </div>
+            <div className="flex flex-1 justify-end">
+                <button type="button"
+                        className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+                        onClick={() => setIsVisible(false)}>
+                    <svg className="size-5 text-gray-900" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                         data-slot="icon">
+                        <path
+                            d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default TopBanner;
