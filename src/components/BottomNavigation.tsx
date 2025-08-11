4d7ec28145b83167c442e9722f28ec1e2cefb935
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2025-08-11 11:43:01 +0200

    fix: fix svg size in bottom navbar

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -79,7 +79,7 @@ const HighlightedBottomNavigationItem: React.FC<HighlightedBottomNavigationItem>
     return (
         <div className="flex items-center justify-center">
             <button onClick={onClick} type="button" className="inline-flex overflow-hidden items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
-                <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox={viewBox}>
+                <svg className="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox={viewBox}>
                     {children}
                 </svg>
                 <span className="sr-only">{item_text}</span>
